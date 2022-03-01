df7dd45408053fc58a084fd0566cfc30f55bf99f
yardpenalty <yardpenalty@yahoo.com>
2022-03-01 14:32:19 -0500

    my_project api controller

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,17 @@
+<?php 
+namespace App\Controller;
+
+use App\Entity\Vehicle;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\COmponent\HttpFoundation\Request;
+
+class VehicleController extends AbstractController
+{
+    public function indexAction(Request $request)
+    {
+        $vehicles = $this->getDoctrine()->getRepository(persistentObject: Vehicle::class)->findAll();
+
+        return $this->json($vehicles);
+    }
+}
+ 
